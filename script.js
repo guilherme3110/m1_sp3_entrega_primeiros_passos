a127ed31b7bf60acb63667f0f7d50c2fca6c5863
guilherme3110 <guilherme311094@hotmail.com>
2022-10-18 20:31:52 -0300

    todas as questões feitas

--- script.js
+++ script.js
@@ -99,21 +99,109 @@
 
 <!-- questão 8 -->  
 
+        let tipo = prompt("Digite o tipo de pulverização ");
+        let area = prompt("Digite o tamanho da area a ser puverizada");
+
+        let erva = 50;
+        let gafanhoto = 100;
+        let broca = 50;
+        let todos = 50;
+
+        let precoFinal ;
+        let custo;
+        let desconto1;
+        let desconto2;
+        let desconto3;
 
 
+        if (tipo == 1) {
+            custo = erva * area };
+            if (area > 1000)
+                alert(`seu custo foi de: ${custo}`);
+                desconto1 = custo * 0.05
+                alert(`Após desconto de 5%, seu desconto foi de: ${desconto1}`)
+                precoFinal = custo - desconto1;
+                alert(`Após desconto de 5%, seu preço foi de: ${precoFinal}`) 
+            else if (custo > 750)  
+                desconto2 = custo * 0.10;
+                precoFinal = custo - desconto2;
+                alert(`Após desconto de 10%, seu preço foi de: ${precoFinal}`)
+            else if ((custo > 750) && (area > 1000))
+                desconto3 =((custo * 0.05) + (custo * 0.10));
+                precoFinal = custo - desconto3
+                alert(`Após desconto de 5% de área mais o 10% de deconto sobre o custo, o valor total foi de: ${precoFinal}`);
 
-
-
+       else if (tipo == 2)
+            custo = gafanhoto * area;
+            if (area > 1000)
+                desconto1 = custo * 0.05
+                precoFinal = custo - desconto1;
+                alert(`Após desconto de 5%, seu preço foi de: ${precoFinal}`)
+            else if (custo > 750)
+                desconto2 = custo * 0.10;
+                precoFinal = custo - desconto2;
+                alert(`Após desconto de 10%, seu preço foi de: ${precoFinal}`)
+            else if ((custo > 750) && (area > 1000))
+                desconto3 =((custo * 0.05) + (custo * 0.10));
+                precoFinal = custo - desconto3
+                alert(`Após desconto de 5% de área mais o 10% de deconto sobre o custo, o valor total foi de: ${precoFinal}`);
+            
+        else if (tipo == 3)
+            custo = broca * area;
+            if (area > 1000)
+                desconto1 = custo * 0.05
+                precoFinal = custo - desconto1;
+                alert(`Após desconto de 5%, seu preço foi de: ${precoFinal}`)
+            else if (custo > 750)
+                desconto2 = custo * 0.10;
+                precoFinal = custo - desconto2;
+                alert(`Após desconto de 10%, seu preço foi de: ${precoFinal}`)
+            else if ((custo > 750) && (area > 1000))
+                desconto3 =((custo * 0.05) + (custo * 0.10));
+                precoFinal = custo - desconto3
+                alert(`Após desconto de 5% de área mais o 10% de deconto sobre o custo, o valor total foi de: ${precoFinal}`);
+            
+       else  if (tipo == 4)
+            custo = todos * area;
+            if (area > 1000)
+                desconto1 = custo * 0.05
+                precoFinal = custo - desconto1;
+                alert(`Após desconto de 5%, seu preço foi de: ${precoFinal}`)
+            else if (custo > 750)
+                desconto2 = custo * 0.10;
+                precoFinal = custo - desconto2;
+                alert(`Após desconto de 10%, seu preço foi de: ${precoFinal}`)
+            else if ((custo > 750) && (area > 1000))
+                desconto3 =((custo * 0.05) + (custo * 0.10));
+                precoFinal = custo - desconto3
+                alert(`Após desconto de 5% de área mais o 10% de deconto sobre o custo, o valor total foi de: ${precoFinal}`);
 
 <!-- questão 9 -->  
 
-
-
-
-
-
-
+        let tempoGasto = prompt("Digite aqui o tempo gasto na viagem");
+        let velocidadeMedia = prompt("Digite aqui a velocidade media");
+        let distancia =  (tempoGasto * velocidadeMedia);
+        let litroUsados = distancia / 12;
+       
+        alert(`Seu veiculo percorreu 
+            ${distancia}km a 
+            ${velocidadeMedia} km/h em 
+            ${tempoGasto}h de viagem e consumiu 
+            ${litroUsados.toFixed(2)}l de gasolina`)
 
 <!-- questão 10 -->  
 
-        let valor = 
+        let valor = prompt("Digite um valor da prestação");
+        let taxa = prompt("Digite o valor da taxa");
+        let tempo = prompt("digite o tempo de atraso da prestação");
+
+        let prestacao = valor + (valor *(taxa/100) * tempo);
+
+            
+
+            if ((tempo == 0) || (tempo != Number.parseInt(tempo)))
+            alert(`Valor inválido o tempo de atraso deve ser em dias`)
+            else 
+            alert(`R$ ${prestacao}`)
+
+        
